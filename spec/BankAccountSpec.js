c86610d722b836ca50febf73982fab2a4ecbaba2
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-04-06 11:11:04 +0100

    Fourth test passing, returns a hard coded object

--- spec/BankAccountSpec.js
+++ spec/BankAccountSpec.js
@@ -12,15 +12,21 @@
     expect(account.currentBalance()).toEqual(0)
   })
 
-  it('can take a deposit and update the balance', function(){
-  	account.deposit(2000)
+  it('can take a deposit with a date and update the balance', function(){
+  	account.deposit(2000, "10-01-2012")
   	expect(account.currentBalance()).toEqual(2000)
   })
 
-  it('can allow a withdrawal and update the balance', function(){
-  	account.deposit(2000)
-  	account.withdraw(500)
+  it('can allow a withdrawal with a date and update the balance', function(){
+  	account.deposit(2000, "10-01-2012")
+  	account.withdraw(500, "14-01-2012")
   	expect(account.currentBalance()).toEqual(1500)  	
   })
+
+  it('will store the deposit and the date as an object in an array', function(){
+  	account.deposit(2000, "10-01-2012")
+  	expect(account.statement()).toEqual([{ date: "10-01-2012", credit: 2000}])
+  })
+
 
 })
--- src/BankAccount.js
+++ src/BankAccount.js
@@ -2,16 +2,24 @@
 
 function BankAccount() {
 	this.balance = 0;
+	this.date;
+	this.deposit;
 }
 
 BankAccount.prototype.currentBalance = function() {		
 	return this.balance;
 };
 
-BankAccount.prototype.deposit = function(amount) {
+BankAccount.prototype.deposit = function(amount, date) {
+	this.date = date;
+	this.deposit = amount;
 	this.balance += amount;
 }
 
-BankAccount.prototype.withdraw = function(amount) {
+BankAccount.prototype.withdraw = function(amount, date) {
 	this.balance -= amount;
 }
+
+BankAccount.prototype.statement = function() {
+	return [{ date: this.date, credit: this.deposit }]
+};
