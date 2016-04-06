6931c569fa8b7cddd5366d8e22382bdd5c3cf2ae
Godfrey Got-Wai Chiu <ggwc82@gmail.com>
2016-04-06 14:34:11 +0100

    Moved printStatement method into Statement class

--- src/Statement.js
+++ src/Statement.js
@@ -11,3 +11,13 @@ Statement.prototype.addDeposit = function(amount, date, balance) {
 Statement.prototype.addWithdrawal = function(amount, date, balance) {
 	this.show.push({ date: date, credit: "", debit: amount, balance: balance })
 };
+Statement.prototype.printStatement = function(){
+  var output = "date || credit || debit || balance\n"
+  this.show.reverse().forEach(function(transaction){
+    output += transaction.date + " || " 
+    + transaction.credit.toString() + " || "
+    + transaction.debit.toString() + " || "
+    + transaction.balance.toString() + "\n"
+  })
+  return output
+}
